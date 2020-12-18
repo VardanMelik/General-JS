e6d3a1bca941935475a41bba5cec6eaac4323fa3
EYE MOTION <eyemotiongamestudio@gmail.com>
2020-12-18 19:50:56 +0400

    update

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 
-// variable
+/* 
+// Variables
+*/
+
+// String type
 const name = 'Eyemotion';
+
+// Integer type
 const x = 100;
 const y = 200;
+
+// Boolean type
+const isProgrammer = true;
+const isTeacher = false;
+
+// CamelCase
+ const firstName = '';
+
+// let
+let age = 28;
+console.log('Age: ' + age + ' ' + isProgrammer);
+
 console.log(name);
 alert(x+y);
